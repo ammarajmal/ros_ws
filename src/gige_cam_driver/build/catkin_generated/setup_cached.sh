7b3d4c7452dd92b69591f3669c7a94b7a0774474
Ammar Ajmal <ammarajml@gmail.com>
2023-02-17 07:48:06 +0500

    add "gige_cam_drivers" & "camera_info_manager_py"

---
+++ src/gige_cam_driver/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ammar/catkin_ws/src/gige_cam_driver/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ammar/catkin_ws/src/gige_cam_driver/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/ammar/.local/bin:/home/ammar/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/ammar/catkin_ws/src/gige_cam_driver/build'
+export PYTHONPATH="/home/ammar/catkin_ws/src/gige_cam_driver/build/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ammar/catkin_ws/src/gige_cam_driver/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/ammar/catkin_ws/src/gige_cam_driver:$ROS_PACKAGE_PATH"
