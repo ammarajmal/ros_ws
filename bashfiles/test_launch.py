4209c758bc960cf400fed611f8c49e86401beb0d
Ammar Ajmal <ammarajml@gmail.com>
2023-03-02 07:14:28 +0900

    git-ignore

--- bashfiles/test_launch.py
+++ bashfiles/test_launch.py
@@ -10,18 +10,25 @@ class LaunchHandle(object):
         self.package = 'gige_cam_driver'
         self.launch_path ='launch'
         self.uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        
         self.cam1_launch_file_path = rospkg.RosPack().get_path('gige_cam_driver') + '/launch/calib1.launch'
-        self.cam1_driver = roslaunch.parent.ROSLaunchParent(self.uuid, [self.cam1_launch_file_path])
+        self.cam1_calib = roslaunch.parent.ROSLaunchParent(self.uuid, [self.cam1_launch_file_path])
+        
         self.cam2_launch_file_path = rospkg.RosPack().get_path('gige_cam_driver') + '/launch/calib2.launch'
-        self.cam2_driver = roslaunch.parent.ROSLaunchParent(self.uuid, [self.cam2_launch_file_path])
+        self.cam2_calib = roslaunch.parent.ROSLaunchParent(self.uuid, [self.cam2_launch_file_path])
+        
+        self.cam3_launch_file_path = rospkg.RosPack().get_path('gige_cam_driver') + '/launch/calib3.launch'
+        self.cam3_calib = roslaunch.parent.ROSLaunchParent(self.uuid, [self.cam3_launch_file_path])
         
     def on_shutdown(self):
-        self.cam1_driver.shutdown()
-        self.cam2_driver.shutdown()
+        self.cam1_calib.shutdown()
+        self.cam2_calib.shutdown()
+        self.cam3_calib.shutdown()
         
     def run(self):
-        self.cam1_driver.start()
-        self.cam2_driver.start()
+        self.cam1_calib.start()
+        self.cam2_calib.start()
+        self.cam3_calib.start()
         
 if __name__ == '__main__':
     launch_handle = LaunchHandle()
