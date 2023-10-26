908b13d7cb441f8616a3ebce19495785ffc83a4d
Ammar Ajmal <ammarajml@gmail.com>
2023-10-27 04:55:41 +0900

    start_stop butons.

--- disp_live/src/gui/_backend_.py
+++ disp_live/src/gui/_backend_.py
@@ -3,6 +3,13 @@ import rospy
 import roslaunch
 import subprocess
 MACHINE_NAME = 'Main Computer'
+themes = {'blue': ("#3B8ED0", "#1F6AA5"),
+          'green': ("#2CC985", "#2FA572"),
+          'dark-blue': ("#3a7ebf", "#1f538d"),
+          'red':("#fa5f5a", "#ba3732")
+          }
+# select value of COLOR_SELECT from (0: blue, 1: green, 2: dark-blue)
+COLOR_SELECT = list(themes.keys())[2]
 
 def is_node_running(node_name):
     try:
@@ -33,42 +40,7 @@ def check_active_topic(nuc_machine):
     all_topics = rospy.get_published_topics()
     return topic_name in [topic[0] for topic in all_topics]
 
-def remote_cam_start_(machine_num, remote_nuc_launch,ros_uuid, processes_, button_):
-    """Starts a remote camera at given machine and updates button"""
-    try:
-        if not check_active_topic(machine_num):
-            print(f"Starting Camera {machine_num} from {MACHINE_NAME}...")
-            camera_launch_args = [f"{remote_nuc_launch}",
-                                    f'launch_nuc:=nuc{machine_num}']
-            roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(camera_launch_args)[0],
-                                camera_launch_args[1:])]
-            nuc_remote_cam_driver = roslaunch.parent.ROSLaunchParent(ros_uuid, roslaunch_file)
-            nuc_remote_cam_driver.start()
-            rospy.sleep(1) # waiting to camera node to start
-            try:
-                if is_node_running(f"/nuc{machine_num}"):
-                    print(f"/nuc{machine_num} is running!")
-                    
-                    processes_[f'nuc{machine_num}_remote_cam_driver'] = nuc_remote_cam_driver
-                    rospy.loginfo(f'NUC {machine_num} Camera started successfully!')
-                    button_(machine_num, 'started')
-            except Exception as excep_camera:
-                print(f"/nuc{machine_num} is NOT running!")
-                rospy.logerr(
-                    f'Error starting nuc{machine_num} camera driver: {str(excep_camera)}')
-        else:
-            try:
-                processes_[f'nuc{machine_num}_remote_cam_driver'].shutdown()
-                processes_.pop(f'nuc{machine_num}_remote_cam_driver', None)
-                rospy.loginfo(f'NUC {machine_num} Camera stopped successfully!')
-            except Exception as excep_camera:
-                print('Camera already running..')
-            finally:
-               # Update button text to indicate that the camera can be started
-                button_(machine_num, 'stopped')
-    except roslaunch.RLException as e:
-        print(e)
-def remote_cam_start_updated(machine_num, remote_nuc_launch,ros_uuid, processes_, button_, start_btn, stop_btn):
+def remote_cam_start_updated(machine_num, remote_nuc_launch,ros_uuid, start_btn, stop_btn):
     """Starts a remote camera at given machine and updates button"""
     try:
         if not check_active_topic(machine_num):
@@ -86,36 +58,46 @@ def remote_cam_start_updated(machine_num, remote_nuc_launch,ros_uuid, processes_
                     
                     # processes_[f'nuc{machine_num}_remote_cam_driver'] = nuc_remote_cam_driver
                     rospy.loginfo(f'NUC {machine_num} Camera started successfully!')
-                    start_btn.configure(fg_color='green')
-                    stop_btn.configure(fg_color='red')
+                    start_btn.configure(fg_color=themes['green'])
+                    stop_btn.configure(fg_color=themes['red'], state='normal')
             except roslaunch.RLException as e:
                 print(f"Error in Starting NUC {machine_num} Camera: {e}")
         else:
             print(f"Camera at NUC {machine_num} is already running..")
-            # try:
-            #     processes_[f'nuc{machine_num}_remote_cam_driver'].shutdown()
-            #     processes_.pop(f'nuc{machine_num}_remote_cam_driver', None)
-            #     rospy.loginfo(f'NUC {machine_num} Camera stopped successfully!')
-            # except Exception as excep_camera:
-            #     print('Camera already running..')
-            # finally:
-            #    # Update button text to indicate that the camera can be started
-            #     button_(machine_num, 'stopped')
     except roslaunch.RLException as e:
         print(e)
 
-
-
-
-
-
-
+def remote_cam_stop_updated(machine_num, remote_nuc_launch, ros_uuid, start_btn, stop_btn):
+    """Stops a remote camera at given machine and updates button"""
+    try:
+        if check_active_topic(machine_num):
+            print(f"Stopping Camera {machine_num} from {MACHINE_NAME}...")
+            kill_ros_node(f"/nuc{machine_num}")
+            # processes_[f'nuc{machine_num}_remote_cam_driver'].shutdown()
+            # processes_.pop(f'nuc{machine_num}_remote_cam_driver', None)
+            rospy.loginfo(f'NUC {machine_num} Camera stopped successfully!')
+            start_btn.configure(fg_color=themes[COLOR_SELECT][0])
+            stop_btn.configure(fg_color=themes[COLOR_SELECT][0], state = 'disabled')
+            
+        else:
+            print(f"Camera at NUC {machine_num} is already stopped..")
+    except roslaunch.RLException as e:
+        print(e)
 def return_nuc_status():
     """ return the status of the nuc """
     return is_node_running("/nuc1"), is_node_running("/nuc2"), is_node_running("/nuc3")
 if __name__ == "__main__":
-    print(check_active_topic(2))
-    if is_node_running("/nuc1"):
-        print("Node is running.")
+    try:
+        print(check_active_topic(2))
+        if is_node_running("/nuc1"):
+            print("Node is running.")
+        else:
+            print("Node is not running.")
+    except ConnectionRefusedError:
+        # Handle the connection error here
+        print("ROS Master not running")
+        # You can log the error or perform other error-handling actions
     else:
-        print("Node is not running.")
+        # Code to execute if no exception is raised
+        print("Connected successfully to the server")
+
